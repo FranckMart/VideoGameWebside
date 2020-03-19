d07d5fe6a7a01ce66cd33ac409f5867dc1296a6e
Franck <franckmart22@gmail.com>
2020-03-19 12:01:15 +0100

    Add doc

---
+++ include/header.php
@@ -0,0 +1,37 @@
+<div id="contentHeaderLeft">
+            <img src="" alt="">
+        </div>
+        <div id="contentHeaderCenter">
+            <ul class="navCategorie">
+                <div class="navheader">
+                    <li>
+                        <h2><a href="">WebsideVideoGame</h2></a>
+                    </li>
+                </div>
+                <div class="navheader">
+                    <li>
+                        <h2><a href="">News</h2></a>
+                    </li>
+                </div>
+                <div class="navheader">
+                    <li>
+                        <h2><a href="">Test</h2></a>
+                    </li>
+                </div>
+            </ul>
+        </div>
+        <div id="contentHeaderRight">
+            <div class="login">
+                <!-- Button trigger modal -->
+                <li><a href="" data-toggle="modal" data-target="#exampleModal"></li>
+                <!--<a target="_blank" href="https://icones8.fr/icons/set/name">Nom icon</a> icon by <a target="_blank" href="https://icones8.fr">Icons8</a>-->
+                <img src="img/login.png" alt="login"></a>
+            </div>
+
+
+            <div class="burger">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+        </div>
