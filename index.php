94781039f6d9a4d5b026ecbfc4754f41405c98f9
Franck <franckmart22@gmail.com>
2020-03-19 20:37:06 +0100

    ajout carrousel

--- index.php
+++ index.php
@@ -2,17 +2,57 @@
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="CSS/accueil.css">
-    <title>Accueil</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="CSS/accueil.css">
+  <title>Accueil</title>
 </head>
 
 <body>
-    <header>
-      <?php include("include/header.php") ?>
-    </header>
+  <header>
+    <?php include("include/header.php") ?>
+  </header>
+  <section>
+    <?php include("include/carousel.php") ?>
+
+    <article>
+      <div id="contentCenter">
+
+        <div class="contentLeft">
+          <ul>
+            <li>News 1News 1News 1News 1News 1News 1News 1News 1</li>
+            <li>News 2News 2News 2News 2News 2News 2News 2News 2</li>
+            <li>News 3News 3News 3News 3News 3News 3News 3News 3</li>
+            <li>News 4News 4News 4News 4News 4News 4News 4News 4</li>
+            <li>News 4News 4News 4News 4News 4News 4News 4News 4</li>
+            <li>News 5News 5News 5News 5News 5News 5News 5News 5</li>
+            <li>News 6News 6News 6News 6News 6News 6News 6News 6</li>
+            <li>News 7News 7News 7News 7News 7News 7News 7News 7</li>
+            <li>News 8News 8News 8News 8News 8News 8News 8News 8</li>
+          </ul>
+
+        </div>
+        <div class="contentRight">
+          <div class="contentRightTOP">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam inventore incidunt saepe minus 
+              aut explicabo eum beatae qui voluptate, ut maxime, alias minima. Fugit sequi dicta numquam minus esse deleniti!
+            </p>
+          </div>
+          <div class="contentRightBottom">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam inventore incidunt saepe minus 
+              aut explicabo eum beatae qui voluptate, ut maxime, alias minima. Fugit sequi dicta numquam minus esse deleniti!
+            </p>
+          </div>
+        </div>
+      </div>
+    </article>
+  </section>
+
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 
 </html>
